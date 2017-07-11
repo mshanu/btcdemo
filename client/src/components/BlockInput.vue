3fe61488b34e47b9e5cc6cfd761ac8f9b9acecda
Shanu M <shanu@IN-CH-Shanu.local>
2017-07-11 17:04:55 +0530

    Shanu: Adding bootstrap vue as dependency to the project

--- client/src/components/BlockInput.vue
+++ client/src/components/BlockInput.vue
@@ -1,15 +1,55 @@
 <template>
-  <div class="row">
-    <div class="col-md-4">a</div>
-    <div class="col-md-4">b</div>
-    <div class="col-md-4">c</div>
-  </div>
+  <b-card header="Block" class="mb-2" title="Block Minning"> 
+    <div class="row">
+      <div class="col-md-6">
+        <label>Previous Hash</label>
+        <b-badge>12312312312312</b-badge> 
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-6">
+        <h4>Transactions</h4>
+        <div class="row">
+          <div class="col-md-6">
+            <b-form-input v-model="block.transactions[0].from" type="number" placeholder="From account"/>
+          </div>
+          <div class="col-md-6">
+            <b-form-input v-model="block.transactions[0].to" type="number" placeholder="To account"/>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-6">
+            <b-form-input v-model="block.transactions[0].from" type="number" placeholder="From account"/>
+          </div>
+          <div class="col-md-6">
+            <b-form-input v-model="block.transactions[0].to" type="number" placeholder="To account"/>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-md-6">
+            <b-form-input v-model="block.transactions[0].from" type="number" placeholder="From account"/>
+          </div>
+          <div class="col-md-6">
+            <b-form-input v-model="block.transactions[0].to" type="number" placeholder="To account"/>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-md-6">
+        <b-form-input v-model="block.nonce" type="number" placeholder="nonce"/>
+      </div>
+    </div>
+
+        </b-card>
 </template>
 <script>
 export default {
   data () {
     return {
-      block: { }
+      block: { transactions: [{}] }
     }
   }
 }
